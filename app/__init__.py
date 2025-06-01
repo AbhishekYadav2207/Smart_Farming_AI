fd700542c7ead0ebe6ac7bdacc9b455a174e77f4
Abhishek Yadav <abhishekpaleru0@gmail.com>
2025-06-01 21:42:33 +0530

    Revert "Feature/ Edit init file to implement posgresql database"
    This reverts commit f9366e5f30121785e05b1acc8a9150a0f783c3ab.

--- app/__init__.py
+++ app/__init__.py
@@ -11,11 +11,9 @@ def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # Initialize database
     db.init_app(app)
     migrate.init_app(app, db)
 
-    # Register blueprints
     from app.auth.routes import auth_bp
     from app.farmer.routes import farmer_bp
     from app.government.routes import govt_bp
@@ -36,3 +34,4 @@ def create_app(config_class=Config):
             return {}
 
     return app
+
