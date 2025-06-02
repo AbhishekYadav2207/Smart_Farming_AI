8b16e09b1702beaabf55200ad994fd33d0f22698
Abhishek Yadav <abhishekpaleru0@gmail.com>
2025-06-02 08:57:29 +0530

    BugFix/ Comment the postgres configuration

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config:
     # DB_PASSWORD = urllib.parse.quote_plus(os.getenv('DB_PASSWORD'))
     # DB_HOST = os.getenv('DB_HOST')
     # DB_PORT = os.getenv('DB_PORT')
-    DB_NAME = os.getenv('DB_NAME')
+    # DB_NAME = os.getenv('DB_NAME')
     # SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///farmers.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
