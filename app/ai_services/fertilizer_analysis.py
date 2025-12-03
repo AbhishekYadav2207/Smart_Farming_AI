ea4c66575455c48516a38c49cf64d74fdc80fa8c
Abhishek Yadav <abhishekpaleru0@gmail.com>
2025-12-03 16:32:28 +0530

    BugFix/ Update gemini model

--- app/ai_services/fertilizer_analysis.py
+++ app/ai_services/fertilizer_analysis.py
@@ -3,7 +3,7 @@ from config import Config
 import json
 
 genai.configure(api_key=Config.GEMINI_API_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-2.0-flash')
 
 def analyze(crop, nitrogen, phosphorus, potassium, ph_level):
     if not crop:
